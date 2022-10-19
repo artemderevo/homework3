f51670d8156f5c2fe9b65e86e2198514c1ed020b
q2q2q8 <art-kash@mail.ru>
2022-10-19 18:09:54 +0300

    первое задание

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int  i = 1000;
+        byte  b = 100;
+        short s = 30000;
+        long l = 1000000;
+        float f = 1.01f;
+        double d = 1.01;
+        System.out.println("Int " + i);
+        System.out.println("Byte " + b);
+        System.out.println("Short " + s);
+        System.out.println("Long " + l);
+        System.out.println("Float " + f);
+        System.out.println("Double " + d);
+
+    }
+
+}

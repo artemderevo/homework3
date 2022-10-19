a6d5e3fb0f88d31fc125158912d835189c2791c6
q2q2q8 <art-kash@mail.ru>
2022-10-19 19:05:58 +0300

    шестое задание

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        int TotalCans = 120;
-        int WhiteCansPerClass = 2;
-        int BrownCansPerClass = 4;
-        int CansPerClass = WhiteCansPerClass + BrownCansPerClass;
-        int TotalClasses = TotalCans / CansPerClass;
-        int TotalWhiteCans = TotalClasses * WhiteCansPerClass;
-        int TotalBrownCans = TotalClasses * BrownCansPerClass;
-        System.out.println("В школе, где " + TotalClasses + " классов, нужно " + TotalWhiteCans + " банок белой краски и " + TotalBrownCans + " банок коричневой краски");
-
+        int Banana = 5 * 80;
+        int Milk = 105 * 2;
+        int IceCream = 100 * 2;
+        int Eggs = 4 * 70;
+        int BreakfastWeightInG = Banana + Milk + IceCream + Eggs;
+        double BreakfastWeightInKG = BreakfastWeightInG / 1000;
+        System.out.println("Вес спортивного завтрака " + BreakfastWeightInG + " в граммах, " + BreakfastWeightInKG + " в килограммах");
 
     }
 

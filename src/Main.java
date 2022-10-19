4c4d04d0abef1b30de400967acef196d2aeb1488
q2q2q8 <art-kash@mail.ru>
2022-10-19 18:57:12 +0300

    пятое задание

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args){
-        int BottlesPer2Minutes = 16;
-        int BottlesPer20Minutes = BottlesPer2Minutes * 10;
-        int BottlesPerDay = BottlesPer20Minutes * 3 * 24;
-        int BottlesPer3Days = BottlesPerDay * 3;
-        int BottlesPer30Days = BottlesPer3Days * 10;
-        System.out.println("За 20 минут машина произвела " + BottlesPer20Minutes + "  бутылок");
-        System.out.println("За 1 день машина произвела " + BottlesPerDay + " бутылок");
-        System.out.println("За 3 дня машинна произвела " + BottlesPer3Days + " бутылок");
-        System.out.println("За месяц машина произвела " + BottlesPer30Days + " бутылок");
+        int TotalCans = 120;
+        int WhiteCansPerClass = 2;
+        int BrownCansPerClass = 4;
+        int CansPerClass = WhiteCansPerClass + BrownCansPerClass;
+        int TotalClasses = TotalCans / CansPerClass;
+        int TotalWhiteCans = TotalClasses * WhiteCansPerClass;
+        int TotalBrownCans = TotalClasses * BrownCansPerClass;
+        System.out.println("В школе, где " + TotalClasses + " классов, нужно " + TotalWhiteCans + " банок белой краски и " + TotalBrownCans + " банок коричневой краски");
 
 
     }

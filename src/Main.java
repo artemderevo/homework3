1bef6a2e493c65cd450347cea4ee77c0119934b1
q2q2q8 <art-kash@mail.ru>
2022-10-19 18:36:30 +0300

    третье задание

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        int  FifthNumber = 569;
-        byte  ThirdNumber = 2;
-        short FourthNumber = 787;
-        float SecondNumber = 987678965549F;
-        boolean False = FifthNumber > FourthNumber;
-        double FirstNumber = 27.12;
-        short SixthNumber = -159;
-        long SeventhNumber = 27897;
-        byte EightNumber = 67;
-        System.out.println(FirstNumber);
-        System.out.println(SecondNumber);
-        System.out.println(ThirdNumber);
-        System.out.println(FourthNumber);
-        System.out.println(FifthNumber);
-        System.out.println(SixthNumber);
-        System.out.println(SeventhNumber);
-        System.out.println(EightNumber);
-        System.out.println(False);
-
+        int NumberOfStudentsLP = 23;
+        int NumberOfStudentsAS = 27;
+        int NumberOfStudentsEA = 30;
+        int TotalSheetsOfPaper = 480;
+        int SheetsOfPaperPerStudent = 480 / (NumberOfStudentsLP + NumberOfStudentsAS + NumberOfStudentsEA);
+        System.out.println("На каждого ученика рассчитано " + SheetsOfPaperPerStudent + " листов бумаги");
 
 
     }

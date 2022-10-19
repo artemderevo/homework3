26306ad4e2aa80b27ed2fb4c1f37922c5a216d81
q2q2q8 <art-kash@mail.ru>
2022-10-19 19:29:28 +0300

    восьмое задание

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 public class Main {
     public static void main(String[] args){
-        int TotalWeight = 7 * 1000;
-        int DaysWithMinWeightLoss = TotalWeight / 250;
-        int DaysWithMaxWeightLoss = TotalWeight / 500;
-        int AverageDaysToLoseWeight = (DaysWithMinWeightLoss + DaysWithMaxWeightLoss) / 2;
-        System.out.println("Спортсмен потратит " + DaysWithMinWeightLoss + " дней с потерей веса 250 граммов в сутки; " + DaysWithMaxWeightLoss + " дней с потерей веса 500 граммов в сутки. В среднем у него уйдет " + AverageDaysToLoseWeight + " день.");
+        int Masha = 67760;
+        int Denis = 83690;
+        int Kristina = 76230;
+        double MashaD = Masha * 0.1 + Masha;
+        double DenisD = Denis * 0.1 + Denis;
+        double KristinaD = Kristina * 0.1 + Kristina;
+        int MashaY = Masha * 12;
+        int DenisY = Denis * 12;
+        int KristinaY = Kristina * 12;
+        double MashaDY = MashaD * 12;
+        double DenisDY = DenisD * 12;
+        double KristinaDY = KristinaD * 12;
+        double MashaPlus = MashaDY - MashaY;
+        double DenisPlus = DenisDY - DenisY;
+        double KristinaPlus = KristinaDY - KristinaY;
+        System.out.println("Маша теперь получает " + MashaD + " рублей. Годовой доход вырос на "+ MashaPlus + " рублей");
+        System.out.println("Денис теперь получает " + DenisD + " рублей. Годовой доход вырос на "+ DenisPlus + " рублей");
+        System.out.println("Кристина теперь получает " + KristinaD + " рублей. Годовой доход вырос на "+ KristinaPlus + " рублей");
+
     }
 
 }

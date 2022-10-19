11ea788336b247ad3f8cebd7482916d106295a76
q2q2q8 <art-kash@mail.ru>
2022-10-19 19:16:07 +0300

    шестое задание

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        int Banana = 5 * 80;
-        int Milk = 105 * 2;
-        int IceCream = 100 * 2;
-        int Eggs = 4 * 70;
-        int BreakfastWeightInG = Banana + Milk + IceCream + Eggs;
-        double BreakfastWeightInKG = BreakfastWeightInG / 1000;
-        System.out.println("Вес спортивного завтрака " + BreakfastWeightInG + " в граммах, " + BreakfastWeightInKG + " в килограммах");
-
+        int TotalWeight = 7 * 1000;
+        int DaysWithMinWeightLoss = TotalWeight / 250;
+        int DaysWithMaxWeightLoss = TotalWeight / 500;
+        int AverageDaysToLoseWeight = (DaysWithMinWeightLoss + DaysWithMaxWeightLoss) / 2;
+        System.out.println("Спортсмен потратит " + DaysWithMinWeightLoss + " дней с потерей веса 250 граммов в сутки; " + DaysWithMaxWeightLoss + " дней с потерей веса 500 граммов в сутки. В среднем у него уйдет " + AverageDaysToLoseWeight + " дней.");
     }
 
 }
